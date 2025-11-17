205e5e871620667864ea6a2ed8e919e4252e026b
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-11-17 03:49:00 +0000

    Rename Django project from config to pbcuk
    Migrates the Django project from 'config' to 'pbcuk' by moving all core project files (settings, asgi, wsgi, urls) to the new pbcuk package. The old config files are now compatibility shims that forward to pbcuk, and manage.py is updated to use pbcuk.settings. This prepares the project for a new canonical package name and structure.

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,2 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_asgi_application()
+"""Compatibility shim: forwards to pbcuk.asgi.application."""
+from pbcuk.asgi import application  # noqa: F401
